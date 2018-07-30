6bf354fd9182d98651edf1224a74616808315788
CarloPunzalan <carlo10punzalan@gmail.com>
2018-07-30 16:09:59 +0800

    updated files

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const { Client } = require('pg');
 const client = new Client({
 	 // database: 'MyDatabase',
@@ -33,6 +35,10 @@ app.set('view engine','handlebars');
 app.set('port',(process.env.PORT|| 3000));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'static1')));
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 app.get('/', function(req, res) {
@@ -83,6 +89,63 @@ app.get('/products/:id', (req,res)=>{
 	// 	res.send('Error!');
 	// });
 // });
+
+app.post('/products/:id/send', function(req, res) {
+	console.log(req.body);
+	var id = req.params.id;
+	const output = `
+		<p>You have a new contact request</p>
+		<h3>Contact Details</h3>
+		<ul>
+			<li>Customer Name: ${req.body.name}</li>
+			<li>Phone: ${req.body.phone}</li>
+			<li>Email: ${req.body.email}</li>
+			<li>Product ID: ${req.body.productid}</li>
+			<li>Quantity ${req.body.quantity}</li>
+		</ul>
+	`;
+
+	//nodemailer
+	let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'team20module1@gmail.com', 
+            pass: 'team20-module1' 
+        }
+    });
+
+    let mailOptions = {
+        from: '"Adidas Mailer" <team20module1@gmail.com>',
+        to: 'carlo10punzalan@gmail.com, kimlesliefaina.klf@gmail.com',
+        subject: 'Adidas Shoes Contact Request',
+        //text: req.body.name,
+        html: output
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        client.query('SELECT * FROM Products', (req, data)=>{
+			var list = [];
+			for (var i = 0; i < data.rows.length+1; i++) {
+				if (i==id) {
+					list.push(data.rows[i-1]);
+				}
+			}
+			res.render('products',{
+				data: list,
+				msg: '---Email has been sent---'
+			});
+		});
+     });
+});
+
 
 //Server
 app.listen(app.get('port'), function() {
