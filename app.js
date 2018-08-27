af91ce45d47e4bd35ad1005dd01c391b7841894e
kimmy0697 <kimlesliefaina.klf@gmail.com>
2018-08-28 02:27:34 +0800

    updated module-4 files

--- app.js
+++ app.js
@@ -324,6 +324,31 @@ app.get('/products/:id', function (req, res) {
     });
 });
 
+
+app.get('/admin/products', function (req, res) {
+  client.query('SELECT products.id AS products_id, products.image AS products_image, products.name AS products_name, products.description AS products_description, products.tagline AS products_tagline, products.price AS products_price, products.warranty AS products_warranty, brands.brand_name AS brand_name, brands.brand_description AS brand_description, products_category.product_category_name AS category_name FROM products INNER JOIN brands ON products.brand_id=brands.id INNER JOIN products_category ON products.category_id=products_category.id WHERE products.id = ' + req.params.id + '; ')
+    .then((results) => {
+      console.log('results?', results);
+      res.render('admin-products', {
+        name: results.rows[0].products_name,
+        description: results.rows[0].products_description,
+        tagline: results.rows[0].products_tagline,
+        price: results.rows[0].products_price,
+        warranty: results.rows[0].products_warranty,
+        image: results.rows[0].products_image,
+        brandname: results.rows[0].brand_name,
+        branddescription: results.rows[0].brand_description,
+        category: results.rows[0].category_name,
+        id: results.rows[0].products_id
+      });
+    })
+    .catch((err) => {
+      console.log('error', err);
+      res.send('Error!');
+    });
+});
+
+
 // Server
 app.listen(app.get('port'), function () {
   console.log('Server started at port 3000');
