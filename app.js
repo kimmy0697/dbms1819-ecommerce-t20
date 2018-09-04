4d25e64232ccd2e085916b215843a6994f8bf128
kimmy0697 <kimlesliefaina.klf@gmail.com>
2018-09-04 23:40:36 +0800

    module-4

--- app.js
+++ app.js
@@ -56,98 +56,8 @@ app.get('/', function (req, res) {
   });
 });
 
-/*********************Admin Side***************************/
 
-app.get('/admin', function (req, res) {
-  res.render('admin/home', {
-    layout: 'admin-layout'
-  });
-});
-
-app.get('/admin/developers', function (req, res) {
-  res.render('admin/developers', {
-    layout: 'admin-layout'
-  });
-});
-
-app.get('/admin/products', function (req, res) {
-  client.query(`SELECT 
-      products.id AS products_id,
-      products.image AS products_image,
-      products.name AS products_name,
-      products.description AS products_description,
-      products.tagline AS products_tagline,
-      products.price AS products_price,
-      products.warranty AS products_warranty,
-      brands.brand_name AS brand_name,
-      brands.brand_description AS brand_description,
-      products_category.product_category_name AS category_name
-    FROM products 
-    INNER JOIN brands ON products.brand_id=brands.id
-    INNER JOIN products_category ON products.category_id=products_category.id`)
-    .then((results) => {
-      console.log('results?', results);
-      res.render('admin/admin-products', {
-        rows: results.rows,
-        layout: 'admin-layout'
-      });
-    })
-    .catch((err) => {
-      console.log('error', err);
-      res.send('Error!');
-    });
-});
-
-
-// app.get('/admin/products', function (req, res) {
-//   Product.list(client, {}, function (products) {
-//     res.render('product',  {
-//       layout: 'admin-layout',
-//       products: products
-//     });
-//   });
-// });
-//   client.query('SELECT products.id AS products_id, products.image AS products_image, products.name AS products_name, products.description AS products_description, products.tagline AS products_tagline, products.price AS products_price, products.warranty AS products_warranty, brands.brand_name AS brand_name, brands.brand_description AS brand_description, products_category.product_category_name AS category_name FROM products INNER JOIN brands ON products.brand_id=brands.id INNER JOIN products_category ON products.category_id=products_category.id WHERE products.id = ' + req.params.id + '; ')
-//     .then((results) => {
-//       console.log('results?', results);
-//       res.render('admin/products-admin', {
-//         rows: results.rows,
-//         layout: 'admin-layout'
-//       });
-//     })
-//     .catch((err) => {
-//       console.log('error', err);
-//       res.send('Error!');
-//     });
-// });
-
-// app.get('/admin/products', function (req, res) {
-//   client.query('SELECT products.id AS products_id, products.image AS products_image, products.name AS products_name, products.description AS products_description, products.tagline AS products_tagline, products.price AS products_price, products.warranty AS products_warranty, brands.brand_name AS brand_name, brands.brand_description AS brand_description, products_category.product_category_name AS category_name FROM products INNER JOIN brands ON products.brand_id=brands.id INNER JOIN products_category ON products.category_id=products_category.id WHERE products.id = ' + req.params.id + '; ')
-//     .then((results) => {
-//       console.log('results?', results);
-//       res.render('admin/admin-products', {
-//         // name: results.rows[0].products_name,
-//         // description: results.rows[0].products_description,
-//         // tagline: results.rows[0].products_tagline,
-//         // price: results.rows[0].products_price,
-//         // warranty: results.rows[0].products_warranty,
-//         // image: results.rows[0].products_image,s
-//         // brandname: results.rows[0].brand_name,
-//         // branddescription: results.rows[0].brand_description,
-//         // category: results.rows[0].category_name,
-//         // id: results.rows[0].products_id,
-//         rows: results.rows,
-//         layout: 'admin-layout'
-//       });
-//     })
-//     .catch((err) => {
-//       console.log('error', err);
-//       res.send('Error!');
-//     });
-// });
-
-
-/*********************Products***************************/
+/*********************Client Products***************************/
 
 // Products List Page
 app.get('/products', function (req, res) {
@@ -305,11 +215,47 @@ app.post('/products/:id/send', function (req, res) {
     });
 });
 
-app.post('/updateproduct/:id', function (req, res) {
-  client.query("UPDATE products SET name = '" + req.body.products_name + "', description = '" + req.body.products_description + "', tagline = '" + req.body.products_tagline + "', price = '" + req.body.products_price + "', warranty = '" + req.body.products_warranty + "',brand_id = '" + req.body.brand_name + "', category_id = '" + req.body.category_name + "', image = '" + req.body.products_image + "'WHERE id = '" + req.params.id + "' ;");
-  client.query("UPDATE brands SET brand_description = '" + req.body.brand_description + "' WHERE id ='" + req.params.id + "';");
 
-  res.render('admin/products');
+/*********************Admin Products***************************/
+
+app.get('/admin', function (req, res) {
+  res.render('admin/home', {
+    layout: 'admin-layout'
+  });
+});
+
+app.get('/admin/developers', function (req, res) {
+  res.render('admin/developers', {
+    layout: 'admin-layout'
+  });
+});
+
+app.get('/admin/products', function (req, res) {
+  client.query(`SELECT 
+      products.id AS products_id,
+      products.image AS products_image,
+      products.name AS products_name,
+      products.description AS products_description,
+      products.tagline AS products_tagline,
+      products.price AS products_price,
+      products.warranty AS products_warranty,
+      brands.brand_name AS brand_name,
+      brands.brand_description AS brand_description,
+      products_category.product_category_name AS category_name
+    FROM products 
+    INNER JOIN brands ON products.brand_id=brands.id
+    INNER JOIN products_category ON products.category_id=products_category.id`)
+    .then((results) => {
+      console.log('results?', results);
+      res.render('admin/admin-products', {
+        rows: results.rows,
+        layout: 'admin-layout'
+      });
+    })
+    .catch((err) => {
+      console.log('error', err);
+      res.send('Error!');
+    });
 });
 
 // Update Product Form Page
@@ -351,6 +297,14 @@ app.get('/admin/product/update/:id', function (req, res) {
       res.send('Error!');
     });
 });
+
+app.post('/updateproduct/:id', function (req, res) {
+  client.query("UPDATE products SET name = '" + req.body.products_name + "', description = '" + req.body.products_description + "', tagline = '" + req.body.products_tagline + "', price = '" + req.body.products_price + "', warranty = '" + req.body.products_warranty + "',brand_id = '" + req.body.brand_name + "', category_id = '" + req.body.category_name + "', image = '" + req.body.products_image + "'WHERE id = '" + req.params.id + "' ;");
+  client.query("UPDATE brands SET brand_description = '" + req.body.brand_description + "' WHERE id ='" + req.params.id + "';");
+
+  res.redirect('admin/products');
+});
+
 
 /*********************Brands***************************/
 
