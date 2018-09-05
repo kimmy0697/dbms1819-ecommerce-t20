0fac20c2ee8a2cddc72216e4e3bb3fba24246067
kimmy0697 <kimlesliefaina.klf@gmail.com>
2018-09-05 23:22:54 +0800

    module-4

--- app.js
+++ app.js
@@ -75,28 +75,6 @@ app.get('/products/:id', function (req, res) {
     res.render('client/product-details', productData);
   });
 });
-//   client.query('SELECT products.id AS products_id, products.image AS products_image, products.name AS products_name, products.description AS products_description, products.tagline AS products_tagline, products.price AS products_price, products.warranty AS products_warranty, brands.brand_name AS brand_name, brands.brand_description AS brand_description, products_category.product_category_name AS category_name FROM products INNER JOIN brands ON products.brand_id=brands.id INNER JOIN products_category ON products.category_id=products_category.id WHERE products.id = ' + req.params.id + '; ')
-//     .then((results) => {
-//       console.log('results?', results);
-//       res.render('client/product-details', {
-//         name: results.rows[0].products_name,
-//         description: results.rows[0].products_description,
-//         tagline: results.rows[0].products_tagline,
-//         price: results.rows[0].products_price,
-//         warranty: results.rows[0].products_warranty,
-//         image: results.rows[0].products_image,
-//         brandname: results.rows[0].brand_name,
-//         branddescription: results.rows[0].brand_description,
-//         category: results.rows[0].category_name,
-//         id: results.rows[0].products_id
-//       });
-//     })
-//     .catch((err) => {
-//       console.log('error', err);
-//       res.send('Error!');
-//     });
-// });
-
 
 // Create Product Page
 app.get('/admin/product/create', function (req, res) {
@@ -226,31 +204,38 @@ app.get('/admin/developers', function (req, res) {
 });
 
 app.get('/admin/products', function (req, res) {
-  client.query(`SELECT 
-      products.id AS products_id,
-      products.image AS products_image,
-      products.name AS products_name,
-      products.description AS products_description,
-      products.tagline AS products_tagline,
-      products.price AS products_price,
-      products.warranty AS products_warranty,
-      brands.brand_name AS brand_name,
-      brands.brand_description AS brand_description,
-      products_category.product_category_name AS category_name
-    FROM products 
-    INNER JOIN brands ON products.brand_id=brands.id
-    INNER JOIN products_category ON products.category_id=products_category.id`)
-    .then((results) => {
-      console.log('results?', results);
-      res.render('admin/admin-products', {
-        rows: results.rows,
-        layout: 'admin-layout'
-      });
-    })
-    .catch((err) => {
-      console.log('error', err);
-      res.send('Error!');
+//   client.query(`SELECT 
+//       products.id AS products_id,
+//       products.image AS products_image,
+//       products.name AS products_name,
+//       products.description AS products_description,
+//       products.tagline AS products_tagline,
+//       products.price AS products_price,
+//       products.warranty AS products_warranty,
+//       brands.brand_name AS brand_name,
+//       brands.brand_description AS brand_description,
+//       products_category.product_category_name AS category_name
+//     FROM products 
+//     INNER JOIN brands ON products.brand_id=brands.id
+//     INNER JOIN products_category ON products.category_id=products_category.id`)
+//     .then((results) => {
+//       console.log('results?', results);
+//       res.render('admin/admin-products', {
+//         rows: results.rows,
+//         layout: 'admin-layout'
+//       });
+//     })
+//     .catch((err) => {
+//       console.log('error', err);
+//       res.send('Error!');
+//     });
+// });
+  Product.list(client, {}, function(products) {
+    res.render('admin/products', {
+      layout: 'admin-layout',
+      products: products
     });
+  });
 });
 
 // Product Details Page
